34437790b60f8a89f1638079462bf2715adc5c60
AlainNshimirimana <alain.nshimirimana@gmail.com>
2022-11-08 11:15:59 -0800

    main menu complete. Working on selection validation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿int amountUsed = 0;
-int num1 = 0;
-int num2 = 0;
+﻿decimal num1 = 0.00m;
+decimal num2 = 0.00m;
+decimal result = 0.00m;
+int numGames = 0;
 string selection;
 
 Console.WriteLine(" CALCULATOR APP ");
@@ -13,20 +14,59 @@ void MainMenu()
 {
     Console.ForegroundColor = ConsoleColor.Cyan;
     Console.WriteLine("A. Addition");
-    Console.WriteLine("B. Subtraction");
-    Console.WriteLine("C. Division");
-    Console.WriteLine("D. Multiplication");
+    Console.WriteLine("S. Subtraction");
+    Console.WriteLine("D. Division");
+    Console.WriteLine("M. Multiplication");
+    Console.WriteLine("E. Exit");
     Console.Write("> ");
     Console.ResetColor();
     selection = Console.ReadLine().ToUpper();
     Console.WriteLine();
 
-    while (selection != "A" || selection != "B" || selection != "C" || selection != "D")
+    while (selection.ToUpper() != "A" || selection.ToUpper() != "S" || selection.ToUpper() != "D" || selection.ToUpper() != "M" || selection.ToUpper() != "E")
     {
-        Console.WriteLine("Invalid selection. Please enter A, B, C, or D");
+        Console.WriteLine("Invalid selection. Please enter A, S, D, M, or E to exit");
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.Write("> ");
         Console.ResetColor();
         selection = Console.ReadLine().ToUpper();
     }
+
+    switch (selection)
+    {
+        case "A":
+            Addition();
+            break;
+        case "S":
+            Subtraction();
+            break;
+        case "D":
+            Division();
+            break;
+        case "M":
+            Multiplication();
+            break;
+        default:
+            break;
+    }
+}
+
+void Multiplication()
+{
+    throw new NotImplementedException();
+}
+
+void Division()
+{
+    throw new NotImplementedException();
+}
+
+void Subtraction()
+{
+    throw new NotImplementedException();
+}
+
+void Addition()
+{
+    throw new NotImplementedException();
 }
